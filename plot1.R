09ea8efa8062be60fea5eef6b9ff9b81faa8045a
Walter de Valk <walterdevalk@gmail.com>
2014-07-08 21:14:07 +0200

    Creating the first plot

--- plot1.R
+++ plot1.R
@@ -1,19 +1,34 @@
-w <- 480
-h <- 480
+## READ THE DATA
 
-# pdf( file=plot1.png, width=w, heigth=h)
-
+# read the data assuming the txt file is located in the current workdir
 d <- read.table( "household_power_consumption.txt", stringsAsFactors=F, sep=";", header=T)
 
+## PREPARE THE DATA
+
+# get the date and time
 datetime <-  paste( d$Date, d$Time)
 d$datetime <- strptime( datetime, "%d/%m/%Y %H:%M:%S")
 
 fdate <- as.POSIXct("2007-02-01 00:00:00")  # from date
 tdate <- as.POSIXct("2007-02-02 24:00:00")  # to date
 
-# subset the data
+# subset the data between the to dates
 d1 <- subset(d, datetime < tdate & datetime >= fdate) 
 
 
+## PLOTTING PART
+
+# dimensions
+w <- 480
+h <- 480
+
+# set file to write to
+png( "plot1.png", width=w, height=h)
+
+# actual plot
+hist( as.numeric(d1$Global_active_power), main="Global Active Power", col='red', xlab="Global Active Power (kilowatts)")
+
+# finish
+dev.off()
 
 
